f20c7ea0c93f3fff3ca07a24450b9e20cf27e57d
bashiryousufy <bashiryousufy@gmail.com>
2021-10-08 12:45:39 +0800

    fixed barchart bugs

--- dist/js/call_rate.js
+++ dist/js/call_rate.js
@@ -1,239 +1,244 @@
 $('#userID').on('change', function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    var selectedUserID = $('#userID').val();
-    if (selectedUserID == 'select_user') {
-        $('#preDay').addClass('bg-light');
-        $('#pre5').addClass('bg-light');
-        $('#preCycle').addClass('bg-light');
-        $('#prevDayCR').html('User not selected!');
-        $('#prev5DayCR').html('User not selected!');
-        $('#cycleCR').html('User not selected!');
-    } else {
-        $.ajax({
-            type: 'POST',
-            data: {
-                userID: selectedUserID,
-                teamID: 1,
-            },
-            url: 'call_rate.php',
-            cache: false,
-            success: function (response) {
-                $('#kpi_cr').html(response);
-                getReachClass(selectedUserID);
-            },
-        });
-    }
+  var selectedUserID = $('#userID').val();
+  if (selectedUserID == 'select_user') {
+    $('#preDay').addClass('bg-light');
+    $('#pre5').addClass('bg-light');
+    $('#preCycle').addClass('bg-light');
+    $('#prevDayCR').html('User not selected!');
+    $('#prev5DayCR').html('User not selected!');
+    $('#cycleCR').html('User not selected!');
+  } else {
+    $.ajax({
+      type: 'POST',
+      data: {
+        userID: selectedUserID,
+        teamID: 1,
+      },
+      url: 'call_rate.php',
+      cache: false,
+      success: function (response) {
+        $('#kpi_cr').html(response);
+        getReachClass(selectedUserID);
+      },
+    });
+  }
 });
 
 function getReachClass(selectedUserID) {
-    $.ajax({
-        type: 'POST',
-        dataType: 'JSON',
-        data: {
-            userID: selectedUserID,
-            teamID: 1,
-        },
-        url: 'reach_class_percentage.php',
-        cache: false,
-        success: function (response) {
-            // Reach Class
-            let classArray = [];
-            response['class'].forEach(function (items) {
-                classArray.push(items);
-            });
+  $.ajax({
+    type: 'POST',
+    dataType: 'JSON',
+    data: {
+      userID: selectedUserID,
+      teamID: 1,
+    },
+    url: 'reach_class_percentage.php',
+    cache: false,
+    success: function (response) {
+      // Reach Class
+      let classArray = [];
+      response['class'].forEach(function (items) {
+        classArray.push(items);
+      });
 
-            let reachClassObject = {};
-            for (var reachClass of classArray) {
-                reachClassObject = reachClass;
-            }
+      let reachClassObject = {};
+      for (var reachClass of classArray) {
+        reachClassObject = reachClass;
+      }
 
-            $('#classReach').empty();
-            for (var key in reachClassObject) {
-                createEmptyDiv(key);
-            }
+      $('#classReach').empty();
+      for (var key in reachClassObject) {
+        createEmptyDiv(key);
+      }
 
-            let classNameArray = [];
-            let classValueArray = [];
-            $('#classReach')
-                .hide()
-                .show(0);
-            for (var key in reachClassObject) {
-                addCircleProgress(reachClassObject[key] / 100, key);
-                classNameArray.push(key);
-                classValueArray.push(reachClassObject[key]);
-            }
-            //creating bar chart
-            
-            if (isCanvasBlank(document.getElementById('reachClassBarChart'))) {
-                $("#reachClassBarChart").empty();
-                barChart(classNameArray, classValueArray);
-            } else {
-                $('#reachClassBarChart').remove();
-                $('#barCharDiv').append("<canvas id='reachClassBarChart'></canvas>");
-                barChart(classNameArray, classValueArray);
-            }
+      let classNameArray = [];
+      let classValueArray = [];
+      $('#classReach').hide().show(0);
+      for (var key in reachClassObject) {
+        addCircleProgress(reachClassObject[key] / 100, key);
+        classNameArray.push(key);
+        classValueArray.push(reachClassObject[key]);
+      }
+      //creating bar chart
 
-            //Class Frequency
-            let freqClass = [];
-            response['freq'].forEach(function (items) {
-                freqClass.push(items);
-            });
+      if (isCanvasBlank(document.getElementById('reachClassBarChart'))) {
+        $('#reachClassBarChart').empty();
+        barChart(classNameArray, classValueArray);
+      } else {
+        $('#reachClassBarChart').remove();
+        $('#barCharDiv').append("<canvas id='reachClassBarChart'></canvas>");
+        barChart(classNameArray, classValueArray);
+      }
 
-            let freqClassObject = {};
-            for (var freq of freqClass) {
-                freqClassObject = freq;
-            }
+      //Class Frequency
+      let freqClass = [];
+      response['freq'].forEach(function (items) {
+        freqClass.push(items);
+      });
 
-            $('#freqClass').empty();
-            var avg_freq = response['avg-freq'];
-            for (const key in freqClassObject) {
-                //change block color based on the avg freq value
-                if (freqClassObject[key] >= avg_freq) {
-                    createNumberBlock(key, freqClassObject[key], 'bg-success');
-                } else if (
-                    freqClassObject[key] >= avg_freq / 2 &&
-                    freqClassObject[key] < avg_freq
-                ) {
-                    createNumberBlock(key, freqClassObject[key], 'bg-danger');
-                } else if (freqClassObject[key] < avg_freq / 2) {
-                    createNumberBlock(key, freqClassObject[key], 'bg-warning');
-                }
-            }
-        },
-    });
+      let freqClassObject = {};
+      for (var freq of freqClass) {
+        freqClassObject = freq;
+      }
+
+      $('#freqClass').empty();
+      var avg_freq = response['avg-freq'];
+      for (const key in freqClassObject) {
+        //change block color based on the avg freq value
+        if (freqClassObject[key] >= avg_freq) {
+          createNumberBlock(key, freqClassObject[key], 'bg-success');
+        } else if (
+          freqClassObject[key] >= avg_freq / 2 &&
+          freqClassObject[key] < avg_freq
+        ) {
+          createNumberBlock(key, freqClassObject[key], 'bg-danger');
+        } else if (freqClassObject[key] < avg_freq / 2) {
+          createNumberBlock(key, freqClassObject[key], 'bg-warning');
+        }
+      }
+    },
+  });
 }
 
 function addCircleProgress(value, id) {
-    let divID = document.getElementById(id);
-    $(divID)
-        .circleProgress({
-            value: value,
-            size: 150,
-            fill: {
-                gradient: ['red', 'orange'],
-            },
-        })
-        .on('circle-animation-progress', function (event, progress, stopValue) {
-            $(this)
-                .parent()
-                .find('strong')
-                .text(String(stopValue.toFixed(2).substr(2)) + '%');
-        });
+  let divID = document.getElementById(id);
+  $(divID)
+    .circleProgress({
+      value: value,
+      size: 150,
+      fill: {
+        gradient: ['red', 'orange'],
+      },
+    })
+    .on('circle-animation-progress', function (event, progress, stopValue) {
+      $(this)
+        .parent()
+        .find('strong')
+        .text(String(stopValue.toFixed(2).substr(2)) + '%');
+    });
 }
 
 function createEmptyDiv(id) {
-    let outterDiv = document.createElement('div');
-    outterDiv.style.cssText = 'margin: 20px;';
+  let outterDiv = document.createElement('div');
+  outterDiv.style.cssText = 'margin: 20px;';
 
-    let newDiv = document.createElement('div');
-    newDiv.setAttribute('id', id);
-    newDiv.style.cssText = 'position: relative;';
+  let newDiv = document.createElement('div');
+  newDiv.setAttribute('id', id);
+  newDiv.style.cssText = 'position: relative;';
 
-    let percentage = document.createElement('strong');
-    percentage.style.cssText =
-        'position: absolute; top: 50px; left: 0px; width: 100%; text-align: center; line-height: 40px; font-size: 30px;';
+  let percentage = document.createElement('strong');
+  percentage.style.cssText =
+    'position: absolute; top: 50px; left: 0px; width: 100%; text-align: center; line-height: 40px; font-size: 30px;';
 
-    let titleNode = document.createElement('span');
-    let title = document.createTextNode(id);
-    titleNode.style.cssText =
-        'display: block; color: red; margin-top: 12px; margin-left: 45px;';
+  let titleNode = document.createElement('span');
+  let title = document.createTextNode(id);
+  titleNode.style.cssText =
+    'display: block; color: red; margin-top: 12px; margin-left: 45px;';
 
-    titleNode.appendChild(title);
+  titleNode.appendChild(title);
 
-    newDiv.appendChild(percentage);
-    newDiv.appendChild(titleNode);
+  newDiv.appendChild(percentage);
+  newDiv.appendChild(titleNode);
 
-    outterDiv.appendChild(newDiv);
+  outterDiv.appendChild(newDiv);
 
-    let currentDiv = document.getElementById('classReach');
-    currentDiv.appendChild(outterDiv);
+  let currentDiv = document.getElementById('classReach');
+  currentDiv.appendChild(outterDiv);
 }
 
 function createNumberBlock(key, value, color) {
-    var outerMostDiv = document.createElement('div');
-    outerMostDiv.className = 'col-lg-3 col-6';
+  var outerMostDiv = document.createElement('div');
+  outerMostDiv.className = 'col-lg-3 col-6';
 
-    var outterDiv = document.createElement('div');
-    outterDiv.className = 'small-box ' + color;
+  var outterDiv = document.createElement('div');
+  outterDiv.className = 'small-box ' + color;
 
-    outterDiv.setAttribute('id', 'freqClassBlock');
+  outterDiv.setAttribute('id', 'freqClassBlock');
 
-    var innerDiv = document.createElement('div');
-    innerDiv.className = 'inner';
+  var innerDiv = document.createElement('div');
+  innerDiv.className = 'inner';
 
-    var textValue = document.createElement('h3');
-    var t = document.createTextNode(value);
-    textValue.appendChild(t);
+  var textValue = document.createElement('h3');
+  var t = document.createTextNode(value);
+  textValue.appendChild(t);
 
-    var title = document.createElement('p');
-    var subT = document.createTextNode(key);
-    title.appendChild(subT);
+  var title = document.createElement('p');
+  var subT = document.createTextNode(key);
+  title.appendChild(subT);
 
-    innerDiv.appendChild(textValue);
-    innerDiv.appendChild(title);
+  innerDiv.appendChild(textValue);
+  innerDiv.appendChild(title);
 
-    outterDiv.appendChild(innerDiv);
+  outterDiv.appendChild(innerDiv);
 
-    outerMostDiv.appendChild(outterDiv);
+  outerMostDiv.appendChild(outterDiv);
 
-    var currentDiv = document.getElementById('freqClass');
+  var currentDiv = document.getElementById('freqClass');
 
-    currentDiv.insertBefore(outerMostDiv, null);
+  currentDiv.insertBefore(outerMostDiv, null);
 }
 function barChart(label, datas) {
-
-    const labels = label;
-    const data = {
-        labels: labels,
-        datasets: [
-            {
-                label: 'Class Reach Percentage',
-                backgroundColor: randomColor(datas.length),
-                borderColor: 'rgb(60, 60, 60)',
-                borderWidth: 1,
-                data: datas,
-            },
-        ],
-    };
-    const config = {
-        type: 'bar',
-        data: data,
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                },
-            },
+  const labels = label;
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        backgroundColor: randomColor(datas.length),
+        borderColor: 'rgb(60, 60, 60)',
+        borderWidth: 1,
+        barPercentage: 0.5,
+        minBarLength: 2,
+        data: datas,
+      },
+    ],
+  };
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
         },
-    };
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: 'Class Reach Percentage %',
+        },
+        legend: {
+          display: false,
+        },
+      },
+    },
+  };
 
-
-
-    let myChart = new Chart(document.getElementById('reachClassBarChart'), config);
-
-
-
+  let myChart = new Chart(
+    document.getElementById('reachClassBarChart'),
+    config
+  );
 }
 
 function randomColor(length) {
-    var colors = [];
-    while (colors.length < length) {
-        do {
-            var color = Math.floor(Math.random() * 1000000 + 1);
-        } while (colors.indexOf(color) >= 0);
-        colors.push('#' + ('000000' + color.toString(16)).slice(-6));
-    }
-    return colors;
+  var colors = [];
+  while (colors.length < length) {
+    do {
+      var color = Math.floor(Math.random() * 1000000 + 1);
+    } while (colors.indexOf(color) >= 0);
+    colors.push('#' + ('000000' + color.toString(16)).slice(-6));
+  }
+  return colors;
 }
 
 // returns true if every pixel's uint32 representation is 0 (or "blank")
 function isCanvasBlank(canvas) {
-    const context = canvas.getContext('2d');
+  const context = canvas.getContext('2d');
 
-    const pixelBuffer = new Uint32Array(
-        context.getImageData(0, 0, canvas.width, canvas.height).data.buffer
-    );
+  const pixelBuffer = new Uint32Array(
+    context.getImageData(0, 0, canvas.width, canvas.height).data.buffer
+  );
 
-    return !pixelBuffer.some(color => color !== 0);
+  return !pixelBuffer.some((color) => color !== 0);
 }
--- index.php
+++ index.php
@@ -274,7 +274,9 @@ $callTarget = $conn->teamCallTarget($teamID);
         <?php include 'components/footer.php';?>
 
         <!-- chart js -->
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.1/chart.min.js"
+            integrity="sha512-Wt1bJGtlnMtGP0dqNFH1xlkLBNpEodaiQ8ZN5JLA5wpc1sUlk/O5uuOMNgvzddzkpvZ9GLyYNa8w2s7rqiTk5Q=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <!-- jQuery -->
         <script src="plugins/jquery/jquery.min.js"></script>
         <script src="dist\js\circle-progress.min.js"></script>
